4fb949fc4e2c5279ccad0528dd0a9f9eaba083eb
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-02-21 21:19:15 +0800

    add license comment

--- src/App/Article/Views/ArticleView.pas
+++ src/App/Article/Views/ArticleView.pas
@@ -1,7 +1,7 @@
 (*!------------------------------------------------------------
  * Fano Web Framework Skeleton Application (https://fanoframework.github.io)
  *
- * @link      https://github.com/fanoframework/fano-elasticsearch
+ * @link      https://fanoframework.github.io
  * @copyright Copyright (c) 2018 Zamrony P. Juhara
  * @license   https://github.com/fanoframework/fano-app/blob/master/LICENSE (GPL 3.0)
  *------------------------------------------------------------- *)
--- tools/clean.cmd
+++ tools/clean.cmd
@@ -1,9 +1,9 @@
 REM------------------------------------------------------------
-REM [[APP_NAME]] ([[APP_URL]])
+REM Fano Framework Elasticsearch Sample Application (https://fanoframework.github.io)
 REM
-REM @link      [[APP_REPOSITORY_URL]]
-REM @copyright Copyright (c) [[COPYRIGHT_YEAR]] [[COPYRIGHT_HOLDER]]
-REM @license   [[LICENSE_URL]] ([[LICENSE]])
+REM @link      https://github.com/fanoframework/fano-elasticsearch
+REM @copyright Copyright (c) 2019 Zamrony P. Juhara
+REM @license   https://github.com/fanoframework/fano-elasticsearch/blob/master/LICENSE (GPL-3.0)
 REM-------------------------------------------------------------
 
 REM------------------------------------------------------
